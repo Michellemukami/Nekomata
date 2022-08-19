4b075cd7b9f6b567be24df78455171ca7524a5d3
Michellemukami <kamimello463@gmail.com>
2022-08-19 22:32:38 +0300

    added profile

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,16 +5,16 @@
             <div class="pb-0 pl-8 flex justify-between">
                 <div>
                     <h1 class="text-3xl body-font font-oswald font-semibold text-gray-50">Latest Manga Updates</h1>
-                    </div>
-               
+                </div>
+
             </div>
             <div class="flex flex-row p-7 pl-8">
-                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
+                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-7">
                     <div v-for="manga in mangas" :key="manga.id">
-                     <a href="#">
-                        <img alt="Placeholder" class="transform transition duration-500 hover:scale-105 block h-56 w-36 rounded-sm" :src="manga.image">
-                        <p class="text-gray-50 text-sm pt-2">{{manga.title}}</p>
-                     </a>
+                        <a href="#">
+                            <img alt="Placeholder" class="transform transition duration-500 hover:scale-105 block h-48 w-40 rounded-sm" :src="manga.image">
+                            <p class="text-gray-50 text-sm pt-2">{{manga.title}}</p>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -25,49 +25,47 @@
             </div>
             <div class="grid grid-cols-1 gap-5">
 
-          
-            <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                <div class="max-w-sm w-full lg:max-w-full lg:flex">
 
-                <div class="h-48 lg:h-auto lg:w-24 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat" style="background-image: url('/src/assets/onepiece.jpg')">
-                </div>
-                <div class="h-20 border-r border-b border-l border-gray-800 lg:border-l-0 lg:border-t lg:border-gray-800 bg-gray-800 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-center bg-no-repeat bg-cover p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-700 w-full antialiased" style="
-  background-image: url('/src/assets/onep.jpg');
-  background-repeat: no-repat;
-  background-size: cover;
-  background-blend-mode: multiply;">
-                    <div>
-                        <div class="mb-8 flex justify-between">
-                            <p class="text-md text-gray-50 flex items-center">
-                                One Piece
-                            </p>
-                            <p class="text-md text-gray-50 pl-32 flex items-center">01</p>
+                    <div class="h-48 lg:h-auto lg:w-24 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat" style="background-image: url('/src/assets/onepiece.jpg')">
+                    </div>
+                    <div class="h-20 border-r border-b border-l border-gray-800 lg:border-l-0 lg:border-t lg:border-gray-800 bg-gray-800 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-center bg-no-repeat bg-cover p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-700 w-full antialiased" style="
+                        background-image: url('/src/assets/onep.jpg');
+                        background-repeat: no-repat;
+                        background-size: cover;
+                        background-blend-mode: multiply;">
+                        <div>
+                            <div class="mb-8 flex justify-between">
+                                <p class="text-md text-gray-50 flex items-center">
+                                    One Piece
+                                </p>
+                                <p class="text-md text-gray-50 pl-32 flex items-center">01</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                  </div>
-                    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                <div class="max-w-sm w-full lg:max-w-full lg:flex">
 
-                <div class="h-48 lg:h-auto lg:w-24 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat" style="background-image: url('/src/assets/kaisen.jpg')">
-                </div>
-                <div class="h-20 border-r border-b border-l border-gray-800 lg:border-l-0 lg:border-t lg:border-gray-800 bg-gray-800 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-center bg-no-repeat bg-cover p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-700 w-full antialiased" style="
-  background-image: url('/src/assets/jjk.jpg');
-  background-repeat: no-repat;
-  background-size: cover;
-  background-blend-mode: multiply;">
-                    <div>
-                        <div class="mb-8 flex justify-between">
-                            <p class="text-md text-gray-50 flex items-center">
-                                Jujutsu Kiasen
-                            </p>
-                            <p class="text-md text-gray-50 pl-24 flex items-center">02</p>
+                    <div class="h-48 lg:h-auto lg:w-24 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat" style="background-image: url('/src/assets/kaisen.jpg')">
+                    </div>
+                    <div class="h-20 border-r border-b border-l border-gray-800 lg:border-l-0 lg:border-t lg:border-gray-800 bg-gray-800 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-center bg-no-repeat bg-cover p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-700 w-full antialiased" style="
+                        background-image: url('/src/assets/jjk.jpg');
+                        background-repeat: no-repat;
+                        background-size: cover;
+                        background-blend-mode: multiply;">
+                        <div>
+                            <div class="mb-8 flex justify-between">
+                                <p class="text-md text-gray-50 flex items-center">
+                                    Jujutsu Kiasen
+                                </p>
+                                <p class="text-md text-gray-50 pl-32 flex items-center">02</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                  </div>
             </div>
         </div>
     </div>
-    
 
 </div>
 </template>
@@ -95,7 +93,7 @@ export default {
                     id: 3,
                     title: 'Bleach',
                     author: 'Jane Doe',
-                    image: '/src/assets/bleach.png',
+                    image: '/src/assets/ichi.jpg',
                     publishedAt: '2016-04-10'
                 },
                 {
@@ -131,6 +129,34 @@ export default {
                     title: 'Tokyo Ghoul',
                     author: 'Jane Doe',
                     image: '/src/assets/tokyo.jpg',
+                    publishedAt: '2016-04-10'
+                },
+                {
+                    id: 9,
+                    title: 'Pokemon',
+                    author: 'Jane Doe',
+                    image: '/src/assets/pikachu.jpg',
+                    publishedAt: '2016-04-10'
+                },
+                {
+                    id: 10,
+                    title: 'Overlord',
+                    author: 'Jane Doe',
+                    image: '/src/assets/ainz.jpg',
+                    publishedAt: '2016-04-10'
+                },
+                {
+                    id: 11,
+                    title: 'Cowboy Bebop',
+                    author: 'Jane Doe',
+                    image: '/src/assets/bebop.jpg',
+                    publishedAt: '2016-04-10'
+                },
+                {
+                    id: 12,
+                    title: 'Dragon Ball',
+                    author: 'Jane Doe',
+                    image: '/src/assets/balls.jpg',
                     publishedAt: '2016-04-10'
                 },
             ]
