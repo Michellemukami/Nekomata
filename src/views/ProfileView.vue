37b09129884d78d23570309b7be22056a9b88c27
Michellemukami <kamimello463@gmail.com>
2022-08-21 16:43:28 +0300

    added profile page

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavBar from '/src/components/NavBar.vue';
+import Footer from '/src/components/Footer.vue';
+import Profile from '../components/Profile.vue';
+</script>
+
+<template>
+    <main class="bg-gray-900">
+    <NavBar />
+    <Profile />
+    <Footer />
+  </main>
+</template>
+
+
